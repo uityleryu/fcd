5d00c42b7b769367cbb41a0fe33e2b0b8608c6ce
Joseph Chou <joseph.chou@ubnt.com>
2018-10-10 20:38:53 -0700

    To update the FW version to 9285.181011.1019

--- include/images.mk
+++ include/images.mk
@@ -19,7 +19,7 @@ USPRO-IMAGE=eb36.bin eb36-mfg.bin \
             images/unifiswitch-us24pro-mfg.bin \
             images/unifiswitch-us48pro-mfg.bin \
             images/US.bcm5616x.feature-usw-pro-dev.9279.181004.1949-uboot-mdk-mfg.bin \
-            images/US.bcm5616x.feature-usw-pro-dev.9282.181006.0923-uboot.bin
+            images/US.bcm5616x.feature-usw-pro-dev.9285.181011.1019-uboot.bin
 
 TOOLS=helper_ARxxxx \
       helper_ARxxxx_musl \
