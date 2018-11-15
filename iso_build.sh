ff30aab9595a71f47747e9b9b4e3766d3ac1c5b4
Lucian Chen <lucian.chen@ubnt.com>
2018-11-15 10:51:55 +0800

    Change docker image name from esxbuild to live-cd.
    Also fix isolinux display text.

--- iso_build.sh
+++ iso_build.sh
@@ -20,7 +20,7 @@ docker run --privileged -it --rm \
     -v "$(dirname $SSH_AUTH_SOCK):/home/ubnt/.ssh-agent:rw" \
     -v "$ENV_DIR:/build:rw" \
     -e "SSH_AUTH_SOCK=/home/ubnt/.ssh-agent/$(basename $SSH_AUTH_SOCK)" \
-    ubnt/esxbuild:fcd \
+    ubnt/live-cd:fcd \
     "$@"
     echo $?
 }
