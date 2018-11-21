9afb58d02a6ea36e4b2eb81dfa8c6c3d7718302c
Lucian Chen <lucian.chen@ubnt.com>
2018-11-21 17:00:48 +0800

    Add config file for Unifi Switch series.

---
+++ include/USW.mk
@@ -0,0 +1,31 @@
+# AFi-AX
+IMAGE-USW-24=images/eb36* \
+             images/eb67* \
+             usw-fw/unifiswitch-us* \
+             usw-fw/US.bcm5616x.feature-usw-pro-dev.*
+
+IMAGE-USW-6XG=images/eb23* \
+              usw-fw/unifiswitch-6xg150-* \
+              usw-fw/US.bcm5616x.*
+
+IMAGE-USW+=$(IMAGE-USW-24)
+IMAGE-USW+=$(IMAGE-USW-6XG)
+
+DIAG_MODEL=afi_ax_r
+
+UPYFCD_VER=4e5fa351ba4db345567dbb335a35e078dacfd3ca
+
+TOOLS=tools/al324-ee \
+      tools/ax_gen_eeprom.py \
+      tools/evtest \
+      tools/helper_AL324_release \
+      tools/helper_IPQ807x_release \
+      tools/ipq807x-aarch64-ee \
+      tools/dropbear_arm64 \
+      tools/dropbearkey_arm64 \
+      tools/ubios-udapi-server.default \
+      tools/sshd_config
+
+$(eval $(call ProductImage,USW,FCD-USW-$(VER)))
+$(eval $(call ProductImage,USW,FCD-USW-6XG-$(VER)))
+$(eval $(call ProductImage,USW,FCD-USW-24-$(VER)))
