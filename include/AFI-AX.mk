5d8d64526604ecf7e525a081df4451a9599f8d3e
MASAN.XU <MASAN.XU@ubnt.com>
2018-11-28 11:42:08 +0800

    Updated AFI-AX.mk for AFi-AX-R/AFi-AX-P 0.0.7 release
    Signed-off-by: MASAN.XU <MASAN.XU@ubnt.com>

--- include/AFI-AX.mk
+++ include/AFI-AX.mk
@@ -11,7 +11,7 @@ IMAGE-AFI-AX+=$(IMAGE-AFI-AX-P)
 
 DIAG_MODEL=afi_ax_r
 
-UPYFCD_VER=4e5fa351ba4db345567dbb335a35e078dacfd3ca
+UPYFCD_VER=4956696e8902c84db6d2b0b69eda60a0ef9da942
 
 TOOLS=tools/al324-ee \
       tools/ax_gen_eeprom.py \
@@ -22,7 +22,10 @@ TOOLS=tools/al324-ee \
       tools/dropbear_arm64 \
       tools/dropbearkey_arm64 \
       tools/ubios-udapi-server.default \
-      tools/sshd_config
+      tools/sshd_config \
+      tools/stop_daemons.sh \
+      tools/DUT_wireless \
+      tools/Golden_wireless
 
 $(eval $(call ProductImage,AFI-AX,FCD-AFI-AX-$(VER)))
 $(eval $(call ProductImage,AFI-AX-R,FCD-AFI-AX-R-$(VER)))
