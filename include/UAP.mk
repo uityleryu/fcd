2bba967fa8f79fe665d20f5337b57eabc7fe14e8
Lucian Chen <lucian.chen@ubnt.com>
2019-05-09 18:30:20 +0800

    Add support UBB.

--- include/UAP.mk
+++ include/UAP.mk
@@ -3,7 +3,14 @@ IMAGE-UAP-FLEX= \
     uap-fw/uap_km-uap-ramips-factory_7559_9984a40_lede-ramips-mt7621-UAP-NANO-HD-initramfs-kernel.bin \
     uap-fw/BZ.mt7621.*
 
+IMAGE-UBB= \
+    images/dc98* \
+    uap-fw/UBB.v0.9.10.40735.190503.1747.bin \
+    uap-fw/ubntubb-u-boot.rom
+
+
 IMAGE-UAP+=$(IMAGE-UAP-FLEX)
+IMAGE-UAP+=$(IMAGE-UBB)
 
 DIAG_MODEL=UniFiAP
 DIAG_UI_MODEL=UniFiAP
@@ -19,9 +26,16 @@ TOOLS-CONFIG= \
     common/sshd_config \
     common/tmux.conf
 
-TOOLS-UAP+=$(TOOLS-CONFIG)
+TOOLS-UBB+= \
+    uap/cfg_part.bin \
+    uap/helper_IPQ40xx \
+    uap/id_rsa \
+    uap/id_rsa.pub
 
-TOOLS-UAP-FLEXHD=$(TOOLS-USW)
+TOOLS-UBB+=$(TOOLS-CONFIG)
+TOOLS-UAP+=$(TOOLS-UBB)
+
 
 $(eval $(call ProductImage,UAP,FCD-UAP-$(VER)))
 $(eval $(call ProductImage,UAP-FLEXHD,FCD-UAP-FLEXHD-$(VER)))
+$(eval $(call ProductImage,UBB,FCD-UBB-$(VER)))
