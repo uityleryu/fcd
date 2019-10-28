4b3532a3f8a12320c6cab28fb166ea2aaaecf502
Lucian Chen <lucian.chen@ubnt.com>
2019-10-28 19:44:19 +0800

    Refine LTU-Series fcd process.

--- config/includes.chroot/usr/local/sbin/af_factory.py
+++ config/includes.chroot/usr/local/sbin/af_factory.py
@@ -48,105 +48,115 @@ class AFAMEFactroy(ScriptBase):
                 time.sleep(5)
                 break
 
-        log_debug("Signing Radio ip=%s, username=%s, password=%s, key=%s, product=%s" 
-            %( self.target_ip, self.username, self.password, self.key, self.product ))
+        log_debug("Signing Radio ip=%s, username=%s, password=%s, product=%s" 
+            %( self.target_ip, self.username, self.password, self.product ))
         ssh = pyssh(self.target_ip, self.username, self.password)
         scp = pyssh(self.target_ip, self.username, self.password)
         (status, stdout) = ssh.login(verbose=False)
 
+        if status == False:
+            error_critical("Device not found or Login fail: %s" % stdout)
+
+        if self.product == 'AF':
+            helper = "/tftpboot/tools/helper_AM18xx"
+            mtd = "/dev/mtdblock7"
+            dump_cmd = "dd if=" + mtd + " of=/var/tmp/EEPROM"
+        elif self.product == 'AME':
+            helper = "/tftpboot/tools/af_ltu5/helper_UBNTAME"
+            mtd = "/dev/mtdblock4"
+            dump_cmd = "/var/tmp/helper -q -o field=flash_eeprom,format=binary,pathname=/var/tmp/EEPROM"
+
+        msg(5, "Download Helper File")
+        ssh.scp(helper, ':/var/tmp/helper')
+        ssh.write_wait('ls -l /var/tmp/helper; chmod 555 /var/tmp/helper')
+        
+        msg(10, "Run Helper File")
+        ssh.write_wait("/var/tmp/helper -c product_class=airfiber,format=hex >/var/tmp/helperfile")
+
+        msg(15, 'Copy helper output file to host')
+        scp.scp(":/var/tmp/helperfile", "/tmp/helperfile")
+        
+        msg(20,'Reading image from flash')
+        ssh.write_wait(dump_cmd, timeout=5)
+
+        msg(25, 'Copying image to computer')
+        scp.scp(':/var/tmp/EEPROM', '/tmp/EEPROM')
+
+        msg(50, 'Signing image')
+        with open("/tmp/helperfile", 'r') as content_file:
+            content = content_file.read()
+        content = content.splitlines()
+
+        log_debug( content[9] + "\n" + content[11] + "\n" + content[12] + "\n" + content[13])
+
+        if self.product == 'AF':
+            cmd = './client_x86_release -h devreg-prod.ubnt.com' \
+                ' -i field=flash_eeprom,format=binary,pathname=/tmp/EEPROM' \
+                ' -o field=flash_eeprom,format=binary,pathname=/tmp/EEPROM_SIGNED' \
+                ' -k '+ passphrase + \
+                ' -i '+content[9] + \
+                ' -i '+content[10] + \
+                ' -i '+content[11] + \
+                ' -i '+content[7] + \
+                '-x '+keypath+'/ca.pem ' + \
+                '-y '+keypath+'/key.pem ' + \
+                '-z '+keypath+'/crt.pem '
+        elif self.product == 'AME':
+            cmd = './client_x86_release -h devreg-prod.ubnt.com' \
+                ' -i field=flash_eeprom,format=binary,pathname=/tmp/EEPROM' \
+                ' -o field=flash_eeprom,format=binary,pathname=/tmp/EEPROM_SIGNED' \
+                ' -o field=result' \
+                ' -k '+passphrase + \
+                ' -i '+content[9] + \
+                ' -i '+content[11 ] + \
+                ' -i '+content[12 ] + \
+                ' -i '+content[13 ] + \
+                ' -x '+keypath+'/ca.pem' + \
+                ' -y '+keypath+'/key.pem' + \
+                ' -z '+keypath+'/crt.pem'
+
+        log_debug(cmd)
+        (output, status ) = run(cmd, withexitstatus=1)
+        log_debug(output.decode("utf-8"))
+
         if status:
-            msg(5, "Download Helper File")
-            if self.product == 'AF':
-                ssh.scp("/tftpboot/tools/helper_AM18xx", ':/var/tmp/helper_AM18xx')
-                ssh.write_wait('ls -l /var/tmp/helper_AM18xx')
-                ssh.write_wait('chmod 555 /var/tmp/helper_AM18xx')
-                msg(10, "Run Helper File")
-                ssh.write_wait("/var/tmp/helper_AM18xx -c product_class=airfiber,format=hex >/var/tmp/helperfile")
-                msg(15, 'Reading helper output file')
-                scp.scp(":/var/tmp/helperfile", "/tmp/helperfile")
-                msg(20,'Reading image from flash')
-                ssh.write_wait("dd if=/dev/mtdblock7 of=/var/tmp/EEPROM",timeout=5)
+            if int(status) == 231:
+                print("Wrong key used? key=%s" % self.key)
+            print("Device Not Signed")
+            ssh.logout()
+            error_critical("Signature Failed Error: %d" % status)
 
-            elif self.product == 'AME':
-                ssh.scp("/tftpboot/tools/af_ltu5/helper_UBNTAME", ':/var/tmp/helper_UBNTAME')
-                ssh.write_wait('ls -l /var/tmp/helper_UBNTAME')
-                ssh.write_wait('chmod 555 /var/tmp/helper_UBNTAME')
-                msg(10, "Run Helper File")
-                ssh.write_wait("/var/tmp/helper_UBNTAME -c product_class=airfiber,format=hex >/var/tmp/helperfile")
-                msg(15, 'Reading helper output file')
-                scp.scp(":/var/tmp/helperfile", "/tmp/helperfile")
-                msg(20,'Reading image from flash')
-                ssh.write_wait("/var/tmp/helper_UBNTAME -q -o field=flash_eeprom,format=binary,pathname=/var/tmp/EEPROM",timeout=5)
+        msg(60, 'Copying signed image to unit')
+        scp.scp('/tmp/EEPROM_SIGNED', ':/var/tmp/EEPROM_SIGNED')
 
-            msg(25, 'Copying image to computer')
-            scp.scp(':/var/tmp/EEPROM', '/tmp/EEPROM')
+        msg(70, "Writing image to flash")
+        if self.product == 'AF':
+            ssh.write_wait("dd of=" + mtd + " if=/var/tmp/EEPROM_SIGNED", timeout=20)
+        elif self.product == 'AME':
+            ssh.write_wait("/var/tmp/helper -q -i field=flash_eeprom,format=binary,pathname=/var/tmp/EEPROM_SIGNED", timeout=20)
 
-            msg(50, 'Signing image key: %s' % self.key)
-            with open("/tmp/helperfile", 'r') as content_file:
-                content = content_file.read()
-            content = content.splitlines()
+        ssh.write_wait("hexdump -C -s 0x0 -n 1000 " + mtd)
+        ssh.write_wait('hexdump -C -s 0x0 -n 1000 /tmp/EEPROM_SIGNED')
+        ssh.write_wait("hexdump -C -s 0xa000 -n 200 " + mtd)
+        ssh.write_wait('hexdump -C -s 0xa000 -n 200 /tmp/EEPROM_SIGNED')
 
-            if self.product == 'AF':
-                cmd = './client_x86_release -h devreg-prod.ubnt.com -i field=flash_eeprom,format=binary,pathname=/tmp/EEPROM -o field=flash_eeprom,format=binary,pathname=/tmp/EEPROM_SIGNED ' \
-                    '-k '+passphrase+' -i '+content[9]+' -i '+content[10]+ ' -i '+content[11]+' -i '+content[7]+'  -x '+keypath+'/ca.pem -y '+keypath+'/key.pem -z '+keypath+'/crt.pem' 
-                #cmd = 'files/client_x64' +' -i field=flash_eeprom,format=binary,pathname=' + tmp_EEPROM + \
-                #' -o field=error_message -o field=flash_eeprom,format=binary,pathname=' + tmp_EEPROM_SIGNED + \
-                #' -k '+passphrase+' -i '+content[9]+' -i '+content[10]+ ' -i '+content[11]+' -i '+content[7]+ \
-                #' -x '+keypath+'/ca.pem -y '+keypath+'/key.pem -z '+keypath+'/crt.pem'
-            
-            elif self.product == 'AME':
-                log_debug("LINE->%s" % content[9])	#field=product_class_id		#print "LINE->%s" % content[7]
-                log_debug("LINE->%s" % content[11])	#field=flash_jedec_id		#print "LINE->%s" % content[9]
-                log_debug("LINE->%s" % content[12])	#field=flash_uid		#print "LINE->%s" % content[10]
-                log_debug("LINE->%s" % content[13])	#field=AM18xx_cpu_rev_id	#print "LINE->%s" % content[11]
-                #cmd = 'client_x86_release -i field=flash_eeprom,format=binary,pathname=EEPROM -o field=flash_eeprom,format=binary,pathname=EEPROM_SIGNED ' \
-                #      '-k '+passphrase+' -i '+content[9]+' -i '+content[10]+ ' -i '+content[11]+' -i '+content[7]+'  -x '+keypath+'/ca.pem -y '+keypath+'/key.pem -z '+keypath+'/crt.pem'
-                cmd = './client_x86_release -h devreg-prod.ubnt.com -i field=flash_eeprom,format=binary,pathname=/tmp/EEPROM -o field=flash_eeprom,format=binary,pathname=/tmp/EEPROM_SIGNED -o field=result ' \
-                     '-k '+passphrase+' -i '+content[9]+' -i '+content[11]+ ' -i '+content[12]+' -i '+content[13]+'  -x '+keypath+'/ca.pem -y '+keypath+'/key.pem -z '+keypath+'/crt.pem'
+        ssh.write_wait("dd if=" + mtd + " of=/tmp/mtdblock4 bs=65536 count=1")
 
-            log_debug(cmd)
-            (output, status ) = run(cmd, withexitstatus=1)
-            log_debug(output.decode("utf-8"))
-            if status:
-                print("Sign cmd:%s" %(cmd))
-                if int(status) == 231:
-                    print("Wrong key used? key=%s" % self.key)
-                print("Radio Not Signed")
-                ssh.logout()
-                error_critical("Signature Failed Error: %d" % status)
-            else:
-                print("Copying signed image to unit")
-                scp.scp('/tmp/EEPROM_SIGNED', ':/var/tmp/EEPROM_SIGNED')
-
-                msg(70, "Writing image to flash")
-                if self.product == 'AF':
-                    ssh.write_wait("dd of=/dev/mtdblock7 if=/var/tmp/EEPROM_SIGNED", timeout=20)
-                elif self.product == 'AME':
-                    ssh.write_wait("/var/tmp/helper_UBNTAME -q -i field=flash_eeprom,format=binary,pathname=/var/tmp/EEPROM_SIGNED", timeout=20)
-
-            if self.product == 'AF':
-                ssh.write_wait("hexdump -C -s 0x0 -n 1000 /dev/mtdblock7")
-                ssh.write_wait("hexdump -C -s 0xa000 -n 200 /dev/mtdblock7")
-            elif self.product == 'AME':
-                ssh.write_wait("hexdump -C -s 0x0 -n 1000 /dev/mtdblock4")
-                ssh.write_wait("hexdump -C -s 0xa000 -n 200 /dev/mtdblock4")
-            
-            cmd = 'hexdump -C -s 0x0 -n 1000 /tmp/EEPROM_SIGNED'
-            (output, status ) = run(cmd, withexitstatus=1)
-            log_debug(output.decode("utf-8"))
-
-            cmd = 'hexdump -C -s 0xa000 -n 200 /tmp/EEPROM_SIGNED'
-            (output, status ) = run(cmd, withexitstatus=1)
-            log_debug(output.decode("utf-8"))
-
-            msg(90, "Radio Signed!")
-            ssh.write_wait("reboot")
-            msg(100, "Process Completed")
-            sys.stdout.flush()
-            time.sleep(10)
-            #ssh.logout()
+        sum1 = ssh.write_wait("md5sum /tmp/EEPROM_SIGNED | cut -f 1 -d ' '").decode("utf-8")
+        sum1 = sum1.splitlines()
+        sum2 = ssh.write_wait("md5sum /tmp/mtdblock4 | cut -f 1 -d ' '").decode("utf-8")
+        sum2 = sum2.splitlines()
+        log_debug(sum1[-1] + " " + sum2[-1])
+        if sum1[-1] == sum2[-1]:
+            msg(90, "Device Signed!")
         else:
-            error_critical("Radio not found: %s" % stdout)
+            error_critical("Check EEPROM data error")
+
+        ssh.write_wait("reboot")
+        msg(100, "Process Completed")
+        sys.stdout.flush()
+        time.sleep(10)
+        #ssh.logout()
         
 #===========================================================================
 #           main entry
