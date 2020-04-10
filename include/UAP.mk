3a1fa4c9d190c9402f9163f5de7f523f39ac8783
archer.tseng <archer.tseng@ubnt.com>
2020-04-10 15:01:47 +0800

    [UAP6] Rename fw in makefile

--- include/UAP.mk
+++ include/UAP.mk
@@ -52,7 +52,7 @@ IMAGE-UAP6= \
     uap-fw/u-boot-mt7621-mfg*.bin \
     uap-fw/kernel-uap6-afi6-7603_7915a*.bin \
     uap-fw/unifi-v1.1.21.53-gcb13e97f_uap6-mt7621-31MB_u-boot.bin \
-    uap-fw/BZ.mt7621.uap6*
+    uap-fw/BZ.mt7621.*
 
 
 IMAGE-UAP+=$(IMAGE-UAP-FLEXHD)
