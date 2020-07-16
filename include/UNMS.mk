8cd9f8202dd14c02c656d3b516b5eeead58682d3
Lucian Chen <lucian.chen@ubnt.com>
2020-07-16 14:35:36 +0800

    Add UNMS-LTE

--- include/UNMS.mk
+++ include/UNMS.mk
@@ -15,9 +15,16 @@ IMAGE-UNMS-R-LITE= \
     images/ee6b* \
     unms-fw/UNMSR.mt7621.* \
 
+IMAGE-UNMS-LTE= \
+    images/dca2* \
+    images/dca3* \
+    unms-fw/LL.qca956x.* \
+    unms-fw/u-boot-with-app.bin \
+
 IMAGE-UNMS+=$(IMAGE-UNMS-R-PRO)
 IMAGE-UNMS+=$(IMAGE-UNMS-S-LITE)
 IMAGE-UNMS+=$(IMAGE-UNMS-R-LITE)
+IMAGE-UNMS+=$(IMAGE-UNMS-LTE)
 # Model
 # This is used for adding an option in the file of BackT1.desktop
 # and Factory.desktop
@@ -47,15 +54,20 @@ TOOLS-UNMS-S-LITE+= unms-slite/*
 TOOLS-UNMS-R-LITE=$(TOOLS-UNMS)
 TOOLS-UNMS-R-LITE+= unms_rlite/*
 
+TOOLS-UNMS-LTE=$(TOOLS-UNMS)
+TOOLS-UNMS-LTE+= unms-lte/*
+
 # Project target
 
 $(eval $(call ProductImage,UNMS,FCD_$(PRD)_UNMS-ALL_$(VER)_$(FWVER)))
 $(eval $(call ProductImage,UNMS-R-PRO,FCD_$(PRD)_UNMS-R-PRO_$(VER)_$(FWVER)))
 $(eval $(call ProductImage,UNMS-R-LITE,FCD_$(PRD)_UNMS-R-LITE_$(VER)_$(FWVER)))
 $(eval $(call ProductImage,UNMS-S-LITE,FCD_$(PRD)_UNMS-S-LITE_$(VER)_$(FWVER)))
+$(eval $(call ProductImage,UNMS-LTE,FCD_$(PRD)_UNMS-LTE_$(VER)_$(FWVER)))
 
 # Project compressed file for RPi FCD host
-$(eval $(call ProductImage,UNMS,FCD_$(PRD)_UNMS-ALL_$(VER)_$(FWVER)))
+$(eval $(call ProductCompress,UNMS,FCD_$(PRD)_UNMS-ALL_$(VER)_$(FWVER)))
 $(eval $(call ProductCompress,UNMS-R-PRO,FCD_$(PRD)_UNMS-R-PRO_$(VER)_$(FWVER)))
 $(eval $(call ProductCompress,UNMS-R-LITE,FCD_$(PRD)_UNMS-R-LITE_$(VER)_$(FWVER)))
 $(eval $(call ProductCompress,UNMS-S-LITE,FCD_$(PRD)_UNMS-S-LITE_$(VER)_$(FWVER)))
+$(eval $(call ProductCompress,UNMS-LTE,FCD_$(PRD)_UNMS-LTE_$(VER)_$(FWVER)))
