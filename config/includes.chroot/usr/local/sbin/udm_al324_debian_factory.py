606bdac73339aee88b8a32ad5944f9b357a63a87
Archer <archer.tseng@ui.com>
2021-03-09 15:20:17 +0800

    [UDM-PRO-SE] 1. Add pub key for fw update 2. Add LCM checking

--- config/includes.chroot/usr/local/sbin/udm_al324_debian_factory.py
+++ config/includes.chroot/usr/local/sbin/udm_al324_debian_factory.py
@@ -22,13 +22,16 @@ class UDM_AL324_DEBIAN_FACTORY(ScriptBase):
         self.helperexe = "helper_AL324_release"
         self.helper_path = "udm"
         self.bomrev = "113-" + self.bom_rev
-        self.username = "root"
+        self.username = "ubnt"
         self.password = "ubnt"
         self.linux_prompt = "#"
 
         # Base path
-        self.tftpdir = self.tftpdir + "/"
-        self.toolsdir = "tools/"
+        tool_name = {
+            'ea2c': "udm_pro_se"
+        }
+
+        self.tool_folder = os.path.join(self.fcd_toolsdir, tool_name[self.board_id])
 
         # switch chip
         self.swchip = {
@@ -52,7 +55,7 @@ class UDM_AL324_DEBIAN_FACTORY(ScriptBase):
 
         # ethernet interface 
         self.netif = {
-            'ea2c': "eth0"
+            'ea2c': "eth9"
         }
 
         self.infover = {
@@ -74,7 +77,7 @@ class UDM_AL324_DEBIAN_FACTORY(ScriptBase):
         self.REGISTER_ENABLE       = True 
         self.FWUPDATE_ENABLE       = False
         self.DATAVERIFY_ENABLE     = True
-        self.LCM_CHECK_ENABLE      = False
+        self.LCM_CHECK_ENABLE      = True
 
     def set_boot_net(self):
         self.pexp.expect_ubcmd(30, self.bootloader_prompt, "setenv ipaddr " + self.dutip)
@@ -83,8 +86,17 @@ class UDM_AL324_DEBIAN_FACTORY(ScriptBase):
         # FIXME: Use SFP port for temp due to FW is not ready
         self.pexp.expect_ubcmd(30, self.bootloader_prompt, "setenv ethact al_eth0")  # # set sfp 0 or 2 for SPF+
 
+    def set_kernel_net(self):
+        self.pexp.expect_lnxcmd(10, self.linux_prompt, "systemctl stop udapi-server udapi-bridge")
+        self.pexp.expect_lnxcmd(10, self.linux_prompt, "ip link set br0 down")
+        self.pexp.expect_lnxcmd(10, self.linux_prompt, "brctl delbr br0")
+
+        self.pexp.expect_lnxcmd(10, self.linux_prompt, "ifconfig {} {}".format(self.netif[self.board_id], self.dutip))
+
+        self.is_network_alive_in_linux(ipaddr=self.dutip)
+
     def update_uboot(self):
-        self.pexp.expect_action(40, "to stop", "\033\033")
+        self.pexp.expect_action(60, "to stop", "\033\033")
         self.set_boot_net()
 
         time.sleep(2)
@@ -121,24 +133,21 @@ class UDM_AL324_DEBIAN_FACTORY(ScriptBase):
             dest=os.path.join(self.tftpdir, "fw-image.bin")
         )
 
+        # copy pub key
+        self.copy_file(
+            source=os.path.join(self.tool_folder, "unas.pub"),
+            dest=os.path.join(self.tftpdir, "unas.pub")
+        )
+
         self.pexp.expect_ubcmd(10, self.bootloader_prompt, "setenv bootargsextra 'server={} factory'".format(self.tftp_server))
         self.pexp.expect_action(10, self.bootloader_prompt, "run bootcmdtftp")
         self.pexp.expect_only(30, "Bytes transferred")
-        self.pexp.expect_only(180, "Welcome to UniFi")
 
     def init_recovery_image(self):
-        self.pexp.expect_lnxcmd(10, self.linux_prompt, "ifconfig {} {}".format(self.netif[self.board_id], self.dutip))
-        postexp = [
-            "64 bytes from",
-            self.linux_prompt
-        ]
-        self.pexp.expect_lnxcmd(10, self.linux_prompt, "ping -c 1 " + self.tftp_server, postexp)        
+        self.set_kernel_net()
 
     def fwupdate(self):
         pass
-
-    def login(self):
-        self.pexp.expect_only(180, "Welcome to UniFi")
 
     def check_info(self):
         self.pexp.expect_lnxcmd(10, self.linux_prompt, "cat /proc/ubnthal/system.info")
@@ -148,7 +157,11 @@ class UDM_AL324_DEBIAN_FACTORY(ScriptBase):
         self.pexp.expect_only(10, self.linux_prompt)
 
     def lcm_fw_ver_check(self):
-        pass
+        self.scp_get(dut_user=self.user, dut_pass=self.password, dut_ip=self.dutip,
+                     src_file=os.path.join(self.tool_folder, "nvr-lcm-tools_1.0.0-14+g34097ad_arm64.deb"),
+                     dst_file=self.dut_tmpdir)
+        self.pexp.expect_lnxcmd(30, self.linux_prompt, "dpkg -i /tmp/nvr-lcm-tools_1.0.0-14+g34097ad_arm64.deb")
+        self.pexp.expect_lnxcmd(30, self.linux_prompt, "/usr/share/lcm-firmware/lcm-fw-info /dev/ttyACM0", post_exp="md5", retry=3)
 
     def run(self):
         """
@@ -175,6 +188,7 @@ class UDM_AL324_DEBIAN_FACTORY(ScriptBase):
             self.boot_recovery_image()
 
         if self.INIT_RECOVERY_IMAGE is True:
+            self.login(self.username, self.password, timeout=240, log_level_emerg=True)
             self.init_recovery_image()
             msg(15, "Boot up to linux console and network is good ...")
 
@@ -193,13 +207,10 @@ class UDM_AL324_DEBIAN_FACTORY(ScriptBase):
             self.check_devreg_data()
             msg(50, "Finish doing signed file and EEPROM checking ...")
 
-        if self.FWUPDATE_ENABLE is True:
-            self.fwupdate()
-            self.login(self.username, self.password, timeout=180, log_level_emerg=True)
-
         if self.DATAVERIFY_ENABLE is True:
             self.pexp.expect_action(10, self.linux_prompt, "reboot -f")  # for correct ubnthal
-            self.login()
+            self.login(self.username, self.password, timeout=180, log_level_emerg=True)
+            self.set_kernel_net()
             self.check_info()
             msg(80, "Succeeding in checking the devreg information ...")
 
--- include/UniFiDream.mk
+++ include/UniFiDream.mk
@@ -98,6 +98,7 @@ TOOLS-UDMLITE+=$(TOOLS-CONFIG)
 TOOLS-UDMLITE+= udm/*
 
 TOOLS-UDMPRO-SE=$(TOOLS-UDM)
+TOOLS-UDMPRO-SE+= udm_pro_se/*
 
 # Project target
 $(eval $(call ProductImage,UDM,FCD_$(PRD)_UDM_$(VER)_$(FWVER)))
