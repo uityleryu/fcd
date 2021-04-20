798d61dd8af5ea923e7a7d8780586a6014d72835
mitch.chang <mitch.chang@ui.com>
2021-04-20 19:12:44 +0800

    [HK] FCD done

--- config/includes.chroot/usr/local/sbin/ufp_efr32_factory.py
+++ config/includes.chroot/usr/local/sbin/ufp_efr32_factory.py
@@ -11,6 +11,12 @@ import time
 import os
 import re
 import traceback
+import base64
+import zlib
+
+from pprint import pformat
+from collections import OrderedDict
+
 
 DOHELPER_ENABLE = True
 REGISTER_ENABLE = True
@@ -38,6 +44,7 @@ class UFPEFR32FactoryGeneral(ScriptBase):
 
         self.mac_check_dict = {
             'a911': True,
+            'a941': True,
             'a912': False,
             'a915': True,
             'a918': True,
@@ -46,6 +53,7 @@ class UFPEFR32FactoryGeneral(ScriptBase):
 
         self.qrcode_dict = {
             'a911': False,
+            'a941': False,
             'a912': False,
             'a915': False,
             'a918': False,
@@ -54,6 +62,16 @@ class UFPEFR32FactoryGeneral(ScriptBase):
 
         self.sku_dict = {
             'a911': True,
+            'a941': False,  # doorlock homekit set sku in FTU
+            'a912': False,
+            'a915': False,
+            'a918': False,
+            'a919': False,
+        }
+
+        self.homekit_dict = {
+            'a911': False,
+            'a941': True,
             'a912': False,
             'a915': False,
             'a918': False,
@@ -63,6 +81,7 @@ class UFPEFR32FactoryGeneral(ScriptBase):
         # number of Ethernet
         self.ethnum = {
             'a911': "0",
+            'a941': "0",
             'a912': "0",
             'a915': "0",
             'a918': "0",
@@ -72,6 +91,7 @@ class UFPEFR32FactoryGeneral(ScriptBase):
         # number of WiFi
         self.wifinum = {
             'a911': "0",
+            'a941': "0",
             'a912': "0",
             'a915': "0",
             'a918': "0",
@@ -81,6 +101,7 @@ class UFPEFR32FactoryGeneral(ScriptBase):
         # number of Bluetooth
         self.btnum = {
             'a911': "1",
+            'a941': "1",
             'a912': "1",
             'a915': "1",
             'a918': "1",
@@ -115,6 +136,35 @@ class UFPEFR32FactoryGeneral(ScriptBase):
         else:
             log_debug("Generating " + self.eebin_path + " files successfully")
 
+        # check if device connect
+        if self.board_id == "a912":
+            self._sense_cmd_before_registration()
+
+        # get uid
+        try:
+            uid_rtv = self.ser.execmd_getmsg("GETUID", ignore=True)
+            log_info('uid_rtv = {}'.format([uid_rtv]))
+            res = re.search(r"UNIQUEID:27-(.*)\n", uid_rtv, re.S)
+            self.uid = res.group(1)
+
+            cpuid_rtv = self.ser.execmd_getmsg("GETCPUID", ignore=True)
+            res = re.search(r"CPUID:(.*)\n", cpuid_rtv, re.S)
+            self.cpuid = res.group(1)
+
+            jedecid_rtv = self.ser.execmd_getmsg("GETJEDEC", ignore=True)
+            res = re.search(r"JEDECID:(.*)\n", jedecid_rtv, re.S)
+            self.jedecid = res.group(1)
+
+        except Exception as e:
+            log_debug("Extract UID, CPUID and JEDEC failed")
+            log_debug("{}".format(traceback.format_exc()))
+            error_critical("{}\n{}".format(sys.exc_info()[0], e))
+
+        log_info('uid = {}'.format(self.uid))
+        log_info('cpuid = {}'.format(self.cpuid))
+        log_info('jedecid = {}'.format(self.jedecid))
+        log_info("Extract UID, CPUID and JEDEC successfully")
+
     def _sense_cmd_before_registration(self):
 
         log_debug("check dut connection".center(60, "="))
@@ -130,35 +180,128 @@ class UFPEFR32FactoryGeneral(ScriptBase):
         self.ser.execmd(cmd=cmd_clr_all_disable)
         time.sleep(2)
 
+    def _check_is_homekit_done_mod_cmd(self, cmd):
+
+        log_debug('_check_is_homekit_done')
+
+        rsp = self.ser.execmd_getmsg("FCDMFGDATAHKTOKENIDGET", ignore=True)
+        res = re.search(r"TOKENID:(.*)\n", rsp, re.S)
+        self.tokenid_dut = res.group(1).upper()
+        log_info('tokenid = {}'.format(self.tokenid_dut))
+
+        rsp = self.ser.execmd_getmsg("FCDMFGDATAHKSETUPCODEGET", ignore=True)
+        res = re.search(r"SETUPCODE:(.*)\n", rsp, re.S)
+        setupcode = res.group(1)
+        log_info('setupcode = {}'.format(setupcode))
+
+        self.is_homekit_done_before = self.tokenid_dut != 'UNKNOWN'
+
+        log_info('DUT has {}done homekit registration before'.format(
+            '' if self.is_homekit_done_before else 'NOT '))
+
+        if self.is_homekit_done_before is True:
+            cmd.append('-i field=last_homekit_device_token_id,format=string,value={}'.format(
+                self.tokenid_dut))
+
+        return cmd
+
+    def __gen_homkit_token_csv_txt(self, client_x86_rsp):
+        def _calculate_crc(info_dict):
+            byte = b''
+            byte += info_dict['product_plan_id'].encode('UTF-8')
+            byte += info_dict['token_id'].encode('UTF-8')
+            try:
+                token_encode = info_dict['token'].encode('UTF-8')
+                byte += base64.b64decode(token_encode)
+            except Exception as e:
+                log_info(e)
+                log_info('calculate_crc fail..')
+                crc32 = 0
+            else:
+                crc32 = hex(zlib.crc32(byte) & 0xffffffff).replace('0x', '')
+            return crc32
+
+        log_info('__gen_homkit_token_csv_txt..')
+
+        '''Devreg server response example:
+            field=flash_eeprom,format=binary,pathname=/tftpboot/e.s.0
+            field=registration_id,format=u_int,value=99099685
+            field=result,format=u_int,value=1
+            field=device_id,format=u_int,value=94429505
+            field=homekit_device_token_id,format=string,value=0C00852350D648C68519AE0EF79F0D7F
+            field=homekit_device_token,format=string,value=MYGrMFACAQECAQEESDBGAiEAqB6jlfVSXyItOGpYO5Cg3zvznl4PIi6eMZre9N5HmJgCIQC90d2p828W5bNhsAmnD+K9RDQ/9xZGfLJl9lImcnonRDBXAgECAgEBBE8xTTAJAgFmAgEBBAEBMBACAWUCAQEECPFcGs94AQAAMBQCAgDJAgEBBAsyMTcwMDEtMDAwNDAYAgFnAgEBBBAMAIUjUNZIxoUZrg73nw1/
+            field=homekit_device_uuid,format=string,value=1f551899-b3d3-4305-aa60-3e64185f7d7a
+            field=homekit_product_plan_id,format=string,value=217001-0004
+        '''
+
+        # prepare data
+        info_dict = OrderedDict()
+        info_dict['token_id'] = None
+        info_dict['token'] = None
+        info_dict['uuid'] = None
+        info_dict['product_plan_id'] = None
+
+        log_info('{}'.format([client_x86_rsp]))
+
+        re_tmp = r"{},format=string,value=(.*)\n"
+        for k, v in info_dict.items():
+            log_info('k = {}'.format(k))
+            regex = re.compile(re_tmp.format(k))
+            info_dict[k] = regex.findall(client_x86_rsp+'\n')[0]
+            log_info('v = {}'.format(info_dict[k]))
+
+        info_dict['crc32'] = _calculate_crc(info_dict)
+        log_info('info_dict = \n{}'.format(pformat(info_dict, indent=4)))
+
+        # gen file
+        file_dir = os.path.join('/home/ubnt/usbdisk', 'LOCK-R_hk_output')
+        if not os.path.exists(file_dir):
+            os.makedirs(file_dir)
+
+        # csv
+        csv_path = os.path.join(file_dir, 'token_{}.csv'.format(self.mac.upper()))
+        with open(csv_path, 'w') as f:
+            f.write('{}, {}, {}, {}'.format(
+                info_dict['product_plan_id'], info_dict['token_id'],
+                info_dict['token'], info_dict['crc32']))
+        # txt
+        txt_path = os.path.join(file_dir, 'uuid_{}.txt'.format(self.mac.upper()))
+        with open(txt_path, 'w') as f:
+            f.write('{}'.format(info_dict['uuid']))
+
+        is_file = os.path.isfile(csv_path) and os.path.isfile(txt_path)
+        log_info('csv_path = {}'.format(csv_path))
+        log_info('txt_path = {}'.format(txt_path))
+        log_info('Token CSV & uuid TXT files generate {}'.format('success' if is_file else 'fail'))
+
+        return is_file
+
+    def __check_tokenid_match(self, client_x86_rsp):
+        regex = re.compile(r'token_id,format=string,value=(.*)')
+        tokenid_client = regex.findall(client_x86_rsp+'\n')[0]
+
+        log_info('tokenid_client = {}'.format(tokenid_client))
+        log_info('tokenid_dut = {}'.format(self.tokenid_dut))
+
+        is_tokenid_match = tokenid_client == self.tokenid_dut
+        log_info('tokenid_dut & tokenid_client are {}match'.format('' if is_tokenid_match else 'NOT '))
+        if is_tokenid_match is False:
+            log_info('So server treat this DUT as first time HK registration')
+            self.__gen_homkit_token_csv_txt(client_x86_rsp)
+
+        return is_tokenid_match
+
+    def homekit_setup_after_registration(self):
+        if self.homekit_dict[self.board_id] is False:
+            return
+
+        if self.is_homekit_done_before is False:
+            self.__gen_homkit_token_csv_txt(self.client_x86_rsp)
+        else:
+            self.__check_tokenid_match(self.client_x86_rsp)
+
     def registration(self):
         log_debug("Starting to do registration ...")
-
-        if self.board_id == "a912":
-            self._sense_cmd_before_registration()
-
-        try:
-            uid_rtv = self.ser.execmd_getmsg("GETUID", ignore=True)
-            res = re.search(r"UNIQUEID:27-(.*)\n", uid_rtv, re.S)
-            uid = res.group(1)
-            log_info('uid = {}'.format(uid))
-
-            cpuid_rtv = self.ser.execmd_getmsg("GETCPUID", ignore=True)
-            res = re.search(r"CPUID:(.*)\n", cpuid_rtv, re.S)
-            cpuid = res.group(1)
-            log_info('cpuid = {}'.format(cpuid))
-
-            jedecid_rtv = self.ser.execmd_getmsg("GETJEDEC", ignore=True)
-            res = re.search(r"JEDECID:(.*)\n", jedecid_rtv, re.S)
-            jedecid = res.group(1)
-            log_info('jedecid = {}'.format(jedecid))
-
-
-        except Exception as e:
-            log_debug("Extract UID, CPUID and JEDEC failed")
-            log_debug("{}".format(traceback.format_exc()))
-            error_critical("{}\n{}".format(sys.exc_info()[0], e))
-
-        log_debug("Extract UID, CPUID and JEDEC successfully")
 
         cmd = "uname -a"
         [sto, rtc] = self.cnapi.xcmd(cmd)
@@ -168,18 +311,18 @@ class UFPEFR32FactoryGeneral(ScriptBase):
             log_debug("Get linux information successfully")
             match = re.findall("armv7l", sto)
             if match:
-                clientbin = "/usr/local/sbin/client_rpi4_release"
+                clientbin = "/usr/local/sbin/client_rpi4_release_hk_20210412"
             else:
                 clientbin = "/usr/local/sbin/client_x86_release_20190507"
 
         cmd = [
             "sudo {0}".format(clientbin),
-            "-h devreg-prod.ubnt.com",
+            '-h stage.udrs.io',
             "-k " + self.pass_phrase,
             "-i field=product_class_id,format=hex,value=" + self.prodclass,
-            "-i field=flash_jedec_id,format=hex,value=" + jedecid,
-            "-i field=flash_uid,format=hex,value=" + uid,
-            "-i field=cpu_rev_id,format=hex,value=" + cpuid,
+            "-i field=flash_jedec_id,format=hex,value=" + self.jedecid,
+            "-i field=flash_uid,format=hex,value=" + self.uid,
+            "-i field=cpu_rev_id,format=hex,value=" + self.cpuid,
             "-i field=flash_eeprom,format=binary,pathname=" + self.eebin_path,
             "-i field=fcd_id,format=hex,value=" + self.fcd_id,
             "-i field=fcd_version,format=hex,value=" + self.sem_ver,
@@ -196,22 +339,28 @@ class UFPEFR32FactoryGeneral(ScriptBase):
             "-y " + self.key_dir + "key.pem",
             "-z " + self.key_dir + "crt.pem"
         ]
+
         if self.qrcode_dict[self.board_id]:
             cmd.append("-i field=qr_code,format=hex,value=" + self.qrhex)
 
-        cmdj = ' '.join(cmd)
+        if self.homekit_dict[self.board_id] is True:
+            cmd = self._check_is_homekit_done_mod_cmd(cmd)
 
-        log_debug(cmdj)
-        clit = ExpttyProcess(self.row_id, cmdj, "\n")
-        clit.expect_only(30, "Ubiquiti Device Security Client")
-        clit.expect_only(30, "Hostname")
-        clit.expect_only(30, "field=result,format=u_int,value=1")
+        cmdj = ' '.join(cmd)
+        log_debug('cmd = \n{}'.format(pformat(cmd, indent=4)))
+        log_debug('cmdj = \n{}'.format(cmdj))
+
+        [self.client_x86_rsp, rtc] = self.fcd.common.xcmd(cmdj)
+        log_debug('client_x86 return code = \n{}'.format(rtc))
+
+        if (int(rtc) > 0):
+            error_critical("client_x86 registration failed!!")
+        else:
+            log_debug("Excuting client_x86 registration successfully")
 
         cmd[2] = "-k " + self.input_args.pass_phrase
         poscmd = ' '.join(cmd)
         print("CMD: \n" + poscmd)
-
-        log_debug("Excuting client_x86 registration successfully")
 
         rtf = os.path.isfile(self.eesign_path)
         if rtf is not True:
@@ -224,7 +373,7 @@ class UFPEFR32FactoryGeneral(ScriptBase):
 
         if self.board_id in ["a912", "a918"]:
             self.ser.execmd_expect("xstartdevreg", "begin upload")
-        elif self.board_id in ["a911", "a915", "a919", "a920"]:
+        elif self.board_id in ["a911", "a941", "a915", "a919", "a920"]:
             self.ser.execmd("xstartdevreg")
             time.sleep(0.5)
 
@@ -247,7 +396,7 @@ class UFPEFR32FactoryGeneral(ScriptBase):
         # it needs to reset for updating the MAC, otherwise the MAC would be like "VER-HW:MAC-ff.ff.ff.ff.ff.ff"
         log_info('Sending the reset command')
         try:
-            rtv_reset = self.ser.execmd_getmsg(self.cmd_reset,ignore=True)
+            rtv_reset = self.ser.execmd_getmsg(self.cmd_reset, ignore=True)
             log_info('rtv_reset = {}'.format(rtv_reset))
         except Exception as e:
             log_info('')
@@ -261,23 +410,24 @@ class UFPEFR32FactoryGeneral(ScriptBase):
         time.sleep(3)
 
     def set_sku(self):
+        if self.sku_dict[self.board_id] is False:
+            log_debug("Skip setting SKU ...")
+            return
+
         # parameters
-        cmd_set_sku = 'FCDSKUSET:{sku}'
-        cmd_get_sku = 'FCDSKUGET'
+        cmd_set_sku = 'FCDSKUSET:{sku}' if self.board_id != 'a941' else 'FCDMFGDATASKUSET:{sku}'
+        cmd_get_sku = 'FCDSKUGET' if self.board_id != 'a941' else 'FCDMFGDATASKUGET'
         expected_rsp = 'SKU: {sku}'
-        region_name_dict = {"World": 'US',
-                            "USA/Canada": 'US',
-                            'EU': 'EU',
-                            "Scandi": 'SCANDI'}
+        region_name_dict = {
+            "World": 'US',
+            "USA/Canada": 'US',
+            'EU': 'EU',
+            "Scandi": 'SCANDI'}
 
         # set_sku
         log_debug("Starting to set SKU")
         log_info("self.sku_dict = {}".format(self.sku_dict))
         log_info('self.region_name = {}'.format(self.region_name))
-
-        if self.sku_dict[self.board_id] is False:
-            log_debug("Skip setting SKU ...")
-            return
 
         if self.region_name not in list(region_name_dict.keys()):
             msg = 'self.region_name ({}) is not in region_name_dict {}, Skip setting SKU ...'
@@ -287,7 +437,9 @@ class UFPEFR32FactoryGeneral(ScriptBase):
         sku_code = region_name_dict[self.region_name]
         cmd_set_sku = cmd_set_sku.format(sku=sku_code)
         log_info("cmd_set_sku = {}".format(cmd_set_sku))
-        self.ser.execmd_getmsg(cmd_set_sku)
+        rsp = self.ser.execmd_getmsg(cmd_set_sku)
+        log_info("rsp = {}".format(rsp))
+
         time.sleep(0.5)
 
         self._reset()
@@ -295,6 +447,8 @@ class UFPEFR32FactoryGeneral(ScriptBase):
         # check SKU
         log_info("cmd_get_sku = {}".format(cmd_get_sku))
         rtv_get_sku = self.ser.execmd_getmsg(cmd_get_sku)
+        log_info("rsp = {}".format(rsp))
+        log_info("rtv_get_sku = {}".format(rtv_get_sku))
 
         expected_rsp = expected_rsp.format(sku=sku_code)
         if expected_rsp not in rtv_get_sku:
@@ -315,7 +469,7 @@ class UFPEFR32FactoryGeneral(ScriptBase):
         else:
             log_info('Have reseted before, skip this time')
 
-        rtv_verison = self.ser.execmd_getmsg(self.cmd_version,ignore=True)
+        rtv_verison = self.ser.execmd_getmsg(self.cmd_version, ignore=True)
 
         version = self._read_version(rtv_verison)
         for key, value in version.items():
@@ -358,16 +512,20 @@ class UFPEFR32FactoryGeneral(ScriptBase):
         if REGISTER_ENABLE is True:
             self.registration()
             msg(40, "Finish doing registration ...")
+
+            self.homekit_setup_after_registration()
+            msg(50, "Finish Homekit setup ...")
+
             self.put_devreg_data_in_dut()
-            msg(50, "Finish doing signed file and EEPROM checking ...")
+            msg(60, "Finish doing signed file and EEPROM checking ...")
 
         if SET_SKU_ENABLE is True:
             self.set_sku()
-            msg(60, "Finish settingSKU ...")
+            msg(70, "Finish setting SKU ...")
 
         if CHECK_MAC_ENABLE is True:
             self.check_mac()
-            msg(70, "Finish checking MAC in DUT ...")
+            msg(80, "Finish checking MAC in DUT ...")
 
         msg(100, "Completing registration ...")
         self.close_fcd()
@@ -416,4 +574,9 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+
+    try:
+        main()
+    except Exception:
+        error_critical(traceback.format_exc())
+
