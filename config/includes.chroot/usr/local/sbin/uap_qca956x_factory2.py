7279ac7d416bb23c2e4f1e5208a465cf426f1e83
Joseph Chou <joseph.chou@ui.com>
2022-01-05 17:28:14 +0800

    e618/e619: to add delay and modify the product name

--- config/includes.chroot/usr/local/sbin/uap_qca956x_factory2.py
+++ config/includes.chroot/usr/local/sbin/uap_qca956x_factory2.py
@@ -154,7 +154,7 @@ class UAPQCA956xFactory2(ScriptBase):
 
         time.sleep(15)  # for stable system
 
-        self.is_network_alive_in_linux(retry=10)
+        self.is_network_alive_in_linux(retry=30)
         self.pexp.expect_action(30, self.linux_prompt, "ifconfig br-lan {}".format(self.dutip))
         time.sleep(3)  # for stable eth
         self.is_network_alive_in_linux(retry=10)
