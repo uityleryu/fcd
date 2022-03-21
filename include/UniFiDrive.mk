3a2c1e8dab8302e52d71d63714e422bd61710fa8
jackcai <you@example.com>
2022-03-21 15:48:11 +0800

    [ea51] Initialize FCD.

---
+++ include/UniFiDrive.mk
@@ -0,0 +1,39 @@
+# Images
+IMAGE-UNAS-Pro= \
+    images/ea51* \
+    unas/unas-pro* \
+    unas/uImage
+
+# Model
+# This is used for adding an option in the file of BackT1.desktop
+# and Factory.desktop
+PRD_MODEL=UniFiDrive
+BACKT1_PRDSRL=$(PRD_MODEL)
+DRVREG_PRDSRL=$(PRD_MODEL)
+
+# Common tools
+TOOLS-CONFIG= \
+    common/sshd_config \
+    common/tmux.conf \
+    common/x86-64k-ee \
+    common/aarch64-rpi4-64k-ee
+
+# Project specific tools
+TOOLS-UNAS+= \
+    unas/al324-ee \
+    unas/helper_UNVR-AL324 \
+    unas/config_gu.sh \
+    unas/enp0s1.network \
+    unas/enp0s2.network
+
+TOOLS-UNAS-Pro+=$(TOOLS-UNAS)
+TOOLS-UNAS-Pro+=$(TOOLS-CONFIG)
+
+# Project target
+$(eval $(call ProductImage,UNAS-Pro,FCD_$(PRD)_UNAS-Pro_$(VER)_$(FWVER)))
+
+# Project compressed file for RPi FCD host
+$(eval $(call ProductCompress,UNAS-Pro,FCD_$(PRD)_UNAS-Pro_$(VER)_$(FWVER)))
+
+# Project compressed type2 file for RPi FCD host
+$(eval $(call ProductCompress2,03751_ea51))
