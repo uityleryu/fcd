d7d0a611ace2bdabc3e2295288b7f72528d5ef9e
Joseph Chou <joseph.chou@ui.com>
2022-07-27 22:09:13 +0800

    a642/a643: to add backtot1 and implement upgrade to shipping FW

--- config/includes.chroot/usr/local/sbin/u6_mt7981_mfg.py
+++ config/includes.chroot/usr/local/sbin/u6_mt7981_mfg.py
@@ -5,71 +5,53 @@ from script_base import ScriptBase
 from PAlib.Framework.fcd.expect_tty import ExpttyProcess
 from PAlib.Framework.fcd.logger import log_debug, log_error, msg, error_critical
 
+'''
+    a642: U6-PLUS
+    a643: U6-LRPLUS
+    a667: UniFi-Express
+'''
+
+
 class U6MT7981MFGGeneral(ScriptBase):
-    """
-    command parameter description for BackToT1
-    command: python3
-    para0:   script
-    para1:   slot ID
-    para2:   UART device number
-    para3:   FCD host IP
-    para4:   system ID
-    para5:   Erase calibration data selection
-    ex: [script, 1, 'ttyUSB1', '192.168.1.19', 'eb23', True]
-    """
     def __init__(self):
         super(U6MT7981MFGGeneral, self).__init__()
-        self.mem_addr = "0x44000000"
-        self.nor_bin = "{}-nor.bin".format(self.board_id)
-        self.emmc_img = "{}-emmc.img".format(self.board_id)
-        self.set_bootloader_prompt("IPQ5018#")
-
-    def update_nor(self):
-        cmd = "sf probe; sf erase 0x0 0x1C0000; sf write {} 0x0 0x1C0000".format(self.mem_addr)
-        log_debug(cmd)
-        self.pexp.expect_action(10, exptxt=self.bootloader_prompt, action=cmd)
-        self.pexp.expect_only(60, "Erased: OK")
-        self.pexp.expect_only(60, "Written: OK")
-
-        if self.erasecal == "True":
-            cal_offset = "0x1C0000"
-            cmd = "sf erase 0x1C0000 0x070000"
-            log_debug("Erase calibration data ...")
-            log_debug(cmd)
-            self.pexp.expect_action(10, exptxt=self.bootloader_prompt, action=cmd)
-            self.pexp.expect_only(60, "Erased: OK")
-
-        if self.erase_devreg == "True":
-            devreg_offset = "0x230000"
-            cmd = "sf erase 0x230000 0x010000"
-            log_debug("Erase devreg data ...")
-            log_debug(cmd)
-            self.pexp.expect_action(10, exptxt=self.bootloader_prompt, action=cmd)
-            self.pexp.expect_only(60, "Erased: OK")
-
-        self.pexp.expect_action(10, exptxt=self.bootloader_prompt, action="reset")
-
-    def update_emmc(self):
-        cmd = "imgaddr={}; source $imgaddr:script; sf probe; sf erase 0x110000 +0x10000".format(self.mem_addr)
-        log_debug(cmd)
-        self.pexp.expect_action(10, exptxt=self.bootloader_prompt, action=cmd)
-        self.pexp.expect_only(60, "Flashing u-boot")
-        self.pexp.expect_only(60, "Flashing rootfs_data")
-        self.pexp.expect_only(60, "Erased: OK")
-        self.pexp.expect_action(10, exptxt=self.bootloader_prompt, action="reset")
+        self.bootloader_prompt = "MT7981"
 
     def stop_uboot(self, timeout=60):
-        self.pexp.expect_action(timeout=timeout, exptxt="Hit any key to stop autoboot|Autobooting in", action= "\x1b\x1b")
+        self.pexp.expect_action(40, "to stop", "\033\033")
 
-    def transfer_img(self, address, filename):
-        img = os.path.join(self.image, filename)
-        img_size = str(os.stat(os.path.join(self.tftpdir, img)).st_size)
-        self.pexp.expect_action(10, self.bootloader_prompt, "tftpb {} {}".format(address, img))
-        self.pexp.expect_only(60, "Bytes transferred = {}".format(img_size))
+    def uboot_update(self):
+        cmd = "sf probe"
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
+        cmd = "sf erase 0x10000 0x80000"
+        self.pexp.expect_ubcmd(30, self.bootloader_prompt, cmd)
+        cmd = "tftpboot 0x46000000 images/{}-bspub.bin".format(self.board_id)
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
+        cmd = "mmc erase 0x3400 0xfff"
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
+        cmd = "mmc write 0x46000000 0x3400 0xfff"
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
+        cmd = "reset"
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
+        self.stop_uboot()
+        self.set_ub_net()
+        self.is_network_alive_in_uboot()
 
-    def t1_image_check(self):
-        self.pexp.expect_only(30, "Starting kernel")
-        self.pexp.expect_lnxcmd(120, "UBNT BSP INIT", "dmesg -n1", "#", retry=0)
+    def bspimg_update(self):
+        cmd = "tftpboot 0x46000000 images/{}-bspfw.bin".format(self.board_id)
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
+        cmd = "mmc erase 0x0 0x23ff"
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
+        cmd = "mmc write 0x46000000 0x0 0x23ff"
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
+        md = "mmc erase 0x3400 0xfff"
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
+        cmd = "mmc write 0x46680000 0x3400 0xfff"
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
+        md = "mmc erase 0x4480 0x90000"
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
+        cmd = "mmc write 0x46890000 0x4480 0x90000"
+        self.pexp.expect_ubcmd(10, self.bootloader_prompt, cmd)
 
     def run(self):
         """
@@ -88,32 +70,15 @@ class U6MT7981MFGGeneral(ScriptBase):
         self.stop_uboot()
         msg(10, 'Stop in uboot...')
 
-        self.set_ub_net(self.premac)
+        self.set_ub_net()
         self.is_network_alive_in_uboot()
         msg(20, 'Network in uboot works ...')
 
-        self.transfer_img(address=self.mem_addr, filename=self.nor_bin)
-        msg(30, 'Transfer NOR done')
-        self.update_nor()
-        msg(40, 'Update NOR done ...')
+        self.uboot_update()
+        msg(30, 'Finshing updating to BSP U-Boot image ...')
 
-        # Update EMMC(kernel)
-        self.stop_uboot()
-        msg(50, 'Stop in uboot...')
-
-        self.set_ub_net(self.premac)
-        self.is_network_alive_in_uboot()
-        msg(60, 'Network in uboot works ...')
-
-        self.transfer_img(address=self.mem_addr, filename=self.emmc_img)
-        msg(70, 'Transfer EMMC done')
-
-        self.update_emmc()
-        msg(80, 'Update EMMC done ...')
-
-        # Check if we are in T1 image
-        self.t1_image_check()
-        msg(90, 'Check T1 image done ...')
+        self.bspimg_update()
+        msg(40, 'Finshing updating to BSP image ...')
 
         msg(100, "Back to T1 has completed")
         self.close_fcd()
