18ef9f84ba6f019ee037937d78095f9213fcb065
Denny Chen <denny.chen@ui.com>
2022-08-10 11:08:15 +0800

    U6-LR, MT7622. set different mac for DUTs

--- config/includes.chroot/usr/local/sbin/u6_mt762x_factory.py
+++ config/includes.chroot/usr/local/sbin/u6_mt762x_factory.py
@@ -412,6 +412,10 @@ class U6MT762xFactory(ScriptBase):
                 self.erase_eefiles()
                 msg(30, "Do helper to get the output file to devreg server ...")
                 self.pexp.expect_lnxcmd(10, self.linux_prompt, "echo 7 > /proc/sys/kernel/printk")
+                self.pexp.expect_lnxcmd(10, self.linux_prompt, "ifconfig br0")
+                self.pexp.expect_lnxcmd(10, self.linux_prompt, f"ifconfig br0 hw ether 18:E8:29:00:0C:2{self.row_id}")
+                time.sleep(1)
+                self.pexp.expect_lnxcmd(10, self.linux_prompt, "ifconfig br0")
                 self.prepare_server_need_files()
 
                 eetxt_dut_path = os.path.join(self.tftpdir, self.eetxt)
