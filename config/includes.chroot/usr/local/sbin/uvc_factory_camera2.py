ff84a94d98f0c6fa20c5d8627dbd6e8657466812
C1189-LAPTOP\Tyler Yu <tyler.yu@ui.com>
2022-08-23 13:49:54 +0800

    [UVC-G5Pro] First edition.

--- config/includes.chroot/usr/local/sbin/uvc_factory_camera2.py
+++ config/includes.chroot/usr/local/sbin/uvc_factory_camera2.py
@@ -18,8 +18,8 @@ import configparser
 PROVISION_EN = True
 DOHELPER_EN = True
 REGISTER_EN = True
-FWUPDATE_EN = False
-DATAVERIFY_EN = False
+FWUPDATE_EN = True
+DATAVERIFY_EN = True
 #REGISTER_EN   = False
 #FWUPDATE_EN   = False
 #DATAVERIFY_EN = False
