c348b3302b4081778bb2fd9c28f3031701d90a14
Tyler Yu <tyler.yu@ui.com>
2023-05-29 12:05:13 +0800

    [UMT-US] Product name added country.
    [UMT] Add HW BOM 08443 for EU SKU.

--- include/UniFiMobility.mk
+++ include/UniFiMobility.mk
@@ -1,2 +1,4 @@
 # Project compressed type2 file for RPi FCD host
 $(eval $(call ProductCompress2,03473_0121))
+$(eval $(call ProductCompress2,08443_0121))
+$(eval $(call ProductCompress2,UM_Tag-SERIES))
