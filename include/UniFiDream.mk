3bd74cc64ff25a2c4686f848301a8583d9bb7e56
Austin Li <austin.li@ui.com>
2023-06-12 11:42:07 +0800

    [ea3d][ea3e]Remove unused method and name rule.

--- include/UniFiDream.mk
+++ include/UniFiDream.mk
@@ -76,15 +76,6 @@ IMAGE-UDW-PRO= \
 IMAGE-UDW-PRO-PU+= \
     images/ea2e* \
     udm-fw/udw-pro-pu/*
-
-
-IMAGE-UDM-ENT+= \
-    images/ea3d* \
-    udm-fw/udm-enterprise/*
-
-IMAGE-UXG-ENT+= \
-    images/ea3e* \
-    udm-fw/uxg-enterprise/*
 # Model
 # This is used for adding an option in the file of BackT1.desktop
 # and Factory.desktop
@@ -145,12 +136,6 @@ TOOLS-UDW-PRO-PU=$(TOOLS-UDM)
 TOOLS-UDW-PRO-PU+= \
     pdu_pro/helper_MT7628_release
 
-TOOLS-UDM-ENT+=$(TOOLS-CONFIG)
-TOOLS-UDM-ENT+= \
-    udm/helper_AL324*
-
-
-
 # Project target
 $(eval $(call ProductImage,UDM-00623,FCD_$(PRD)_UDM_$(VER)_$(FWVER)))
 $(eval $(call ProductImage,UDM-01134,FCD_$(PRD)_UDM_$(VER)_$(FWVER)))
@@ -166,8 +151,7 @@ $(eval $(call ProductImage,UDM-SE,FCD_$(PRD)_UDM-SE_$(VER)_$(FWVER)))
 $(eval $(call ProductImage,UDW,FCD_$(PRD)_UDW_$(VER)_$(FWVER)))
 $(eval $(call ProductImage,UDW-PRO,FCD_$(PRD)_UDW_$(VER)_$(FWVER)))
 $(eval $(call ProductImage,UDW-PRO-PU,FCD_$(PRD)_UDW-PRO-PU_$(VER)_$(FWVER)))
-$(eval $(call ProductCompress,UDM-ENT,FCD_$(PRD)_UDM-ENT_$(VER)_$(FWVER)))
-$(eval $(call ProductCompress,UXG-ENT,FCD_$(PRD)_UXG-ENT_$(VER)_$(FWVER)))
+
 # Project compressed file for RPi FCD host
 $(eval $(call ProductCompress,UDM-00623,FCD_$(PRD)_UDM_$(VER)_$(FWVER)))
 $(eval $(call ProductCompress,UDM-01134,FCD_$(PRD)_UDM_$(VER)_$(FWVER)))
@@ -182,8 +166,6 @@ $(eval $(call ProductCompress,UDM-SE,FCD_$(PRD)_UDM-SE_$(VER)_$(FWVER)))
 $(eval $(call ProductCompress,UDW,FCD_$(PRD)_UDW_$(VER)_$(FWVER)))
 $(eval $(call ProductCompress,UDW-PRO,FCD_$(PRD)_UDW_$(VER)_$(FWVER)))
 $(eval $(call ProductCompress,UDW-PRO-PU,FCD_$(PRD)_UDW-PRO-PU_$(VER)_$(FWVER)))
-$(eval $(call ProductCompress,UDM-ENT,FCD_$(PRD)_UDM-ENT_$(VER)_$(FWVER)))
-$(eval $(call ProductCompress,UXG-ENT,FCD_$(PRD)_UXG-ENT_$(VER)_$(FWVER)))
 
 # Project compressed type2 file for RPi FCD host
 
@@ -203,4 +185,5 @@ $(eval $(call ProductCompress2,00843_ea2a))
 $(eval $(call ProductCompress2,00845_ea2b))
 $(eval $(call ProductCompress2,01100_ea3d))
 $(eval $(call ProductCompress2,01218_ea3e))
+
 $(eval $(call ProductCompress2,UniFiDream_UDMSE-SERIES))
