418b3bf4365aa7988306abe5b7c7e40404a272d1
Joyce Jian <joyce.jian@ui.com>
2023-06-12 17:06:31 +0800

    [UA-Extender] FCD 2nd edition - build series

--- include/UniFiAccess.mk
+++ include/UniFiAccess.mk
@@ -95,10 +95,10 @@ $(eval $(call ProductImage,UA-Fob,FCD_$(PRD)_UA-Fob_$(VER)_$(FWVER)))
 $(eval $(call ProductImage,UA-G2-Reader-Pro,FCD_$(PRD)_UA-G2-Reader-Pro_$(VER)_$(FWVER)))
 $(eval $(call ProductImage,UA-ELEVATOR-EXTENDER-TX,FCD_$(PRD)_UA-ELEVATOR-EXTENDER-TX_$(VER)_$(FWVER)))
 $(eval $(call ProductImage,UA-ELEVATOR-EXTENDER-RX,FCD_$(PRD)_UA-ELEVATOR-EXTENDER-RX_$(VER)_$(FWVER)))
+$(eval $(call ProductImage,UA-EXTENDER-SERIES,FCD_$(PRD)_UA-EXTENDER-SERIES_$(VER)_$(FWVER)))
 
 $(eval $(call ProductImage2,03034_ec3e))
 $(eval $(call ProductImage2,03035_ec3f))
-$(eval $(call ProductImage2,UA_EXTENDER-SERIES))
 
 # Project compressed file for RPi FCD host
 $(eval $(call ProductCompress,UA-GATE,FCD_$(PRD)_UA-GATE_$(VER)_$(FWVER)))
