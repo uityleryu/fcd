569994a2559684ecf4309ec0cf3434eba22c827b
doublec <double.chen@ui.com>
2023-08-18 10:50:32 +0800

    [U7-series] add U7.mk

---
+++ include/UniFiAP7.mk
@@ -0,0 +1,7 @@
+
+# Project compressed type2 file for RPi FCD host
+
+$(eval $(call ProductCompress2,01272_a681))
+$(eval $(call ProductCompress2,08146_a682))
+$(eval $(call ProductCompress2,01273_a685))
+$(eval $(call ProductCompress2,U7_IPQ5322-SERIES))
