71b76bc06f52f9f3f5e28c3c5046198be0684aac
Kevin Wen <kevin.wen@ui.com>
2023-11-21 15:25:55 +0800

    [UA-G2-Flex] FCD

--- config/includes.chroot/usr/local/sbin/ua_esp32_factory.py
+++ config/includes.chroot/usr/local/sbin/ua_esp32_factory.py
@@ -58,7 +58,7 @@ class UAESP32FactoryGeneral(ScriptBase):
         # number of Bluetooth
         self.btnum = {
             'ec84': "0",
-            'ec87': "0",
+            'ec87': "1",
         }
 
         self.flash_size = {
